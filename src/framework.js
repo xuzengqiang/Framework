3b70e6f837cb5fa78edcb325eb31f21eccf6bc48
xuzengqiang <253948113@qq.com>
2018-05-01 14:00:50 +0800

    新增karma+jasmine
    新增单元测试依赖

--- src/framework.js
+++ src/framework.js
@@ -6,42 +6,42 @@
  * @version 1.0.0
  */
 (function(global, factory) {
-    "use strict";
+	"use strict";
 
-    factory(global);
+	factory(global);
 })(typeof window !== "undefined" ? window : this, function(window) {
-    var document = window.document;
-    var location = window.location;
-    var rint = /^[1-9]\d*$/;
-    var objProto = Object.prototype;
-    var toString = objProto.toString;
-    var Framework = {};
+	var document = window.document;
+	var location = window.location;
+	var rint = /^[1-9]\d*$/;
+	var objProto = Object.prototype;
+	var toString = objProto.toString;
+	var Framework = {};
 
-    /**
-     * 判断是否为正整数
-     * @param {mixed} number - 需要验证的数据
-     * @since 1.0.0
-     */
-    Framework.isInt = function(number) {
-        return rint.test(number);
-    };
+	/**
+	 * 判断是否为正整数
+	 * @param {mixed} number - 需要验证的数据
+	 * @since 1.0.0
+	 */
+	Framework.isInt = function(number) {
+		return rint.test(number);
+	};
 
-    /**
-     * 判断是否为数字
-     * @param {mixed} number - 需要验证的数据
-     * @since 1.0.0
-     */
-    Framework.isNumber = function(number) {
-        return (
-            (toString.call(number) === "[Object Number]" ||
-                toString.call(number) === "[Object String]") &&
-            !isNaN(number - parseFloat(number))
-        );
-    };
+	/**
+	 * 判断是否为数字
+	 * @param {mixed} number - 需要验证的数据
+	 * @since 1.0.0
+	 */
+	Framework.isNumber = function(number) {
+		return (
+			(toString.call(number) === "[Object Number]" ||
+				toString.call(number) === "[Object String]") &&
+			!isNaN(number - parseFloat(number))
+		);
+	};
 
-    /**
-     * 对外暴露接口
-     * @since 1.0.0
-     */
-    window.Framework = Framework;
+	/**
+	 * 对外暴露接口
+	 * @since 1.0.0
+	 */
+	window.Framework = Framework;
 });
